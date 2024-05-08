dd8773009b82cf6792d416a9a4ea262a4bc561ea
yrysNM <yrys.n3m@gmail.com>
2024-05-08 17:04:27 +0500

    feat(Rating): change style rating

--- src/pages/Rating.tsx
+++ src/pages/Rating.tsx
@@ -4,6 +4,7 @@ import "../assets/styles/pages/rating.scss"
 import Model1 from "../assets/images/leaderboard/medal-1.svg"
 import Model2 from "../assets/images/leaderboard/medal-2.svg"
 import Model3 from "../assets/images/leaderboard/medal-3.svg"
+import { CloudImage } from "../components/CloudImage"
 
 export interface IUser {
     id: string
@@ -50,21 +51,30 @@ export const Rating = () => {
                 </div>
                 <div className="ratingBox">
                     <div className="topPosition">
-                        <span className="topJPID">{topTen.length >= 3 ? topTen[2].fullName : ""}</span>
+                        <div className="topUserBlock">
+                            <CloudImage url={topTen[2].avatar} className="avatar-img" />
+                            <span className="topJPID">{topTen.length >= 3 ? topTen[2].fullName : ""}</span>
+                        </div>
                         <div className="topPositionBox" style={{ height: 50, backgroundColor: "#cd7f32" }}>
                             <img src={Model3} className="rateImg" />
                         </div>
                     </div>
 
                     <div className="topPosition">
-                        <span className="topJPID">{topTen.length >= 1 ? topTen[0].fullName : ""}</span>
+                        <div className="topUserBlock">
+                            <CloudImage url={topTen[0].avatar} className="avatar-img" />
+                            <span className="topJPID">{topTen.length >= 1 ? topTen[0].fullName : ""}</span>
+                        </div>
                         <div className="topPositionBox" style={{ height: 85, backgroundColor: "#e5b80b" }}>
                             <img src={Model1} className="rateImg" />
                         </div>
                     </div>
 
                     <div className="topPosition">
-                        <span className="topJPID">{topTen.length >= 2 ? topTen[1].fullName : ""}</span>
+                        <div className="topUserBlock">
+                            <CloudImage url={topTen[1].avatar} className="avatar-img" />
+                            <span className="topJPID">{topTen.length >= 2 ? topTen[1].fullName : ""}</span>
+                        </div>
                         <div className="topPositionBox" style={{ height: 65, backgroundColor: "silver" }}>
                             <img src={Model2} className="rateImg" />
                         </div>
@@ -77,10 +87,11 @@ export const Rating = () => {
                     .filter((item) => item.fullName.length)
                     .map((item, index) => (
                         <div key={item.id} className="rating-block">
-                            <div>
-                                <p>{item.fullName}</p>
+                            <div style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 5 }}>
+                                <CloudImage url={item.avatar} className="userAvatar" />
+                                <p className="userName">{item.fullName}</p>
                             </div>
-                            <p>{index + 4}</p>
+                            <p className="userPositionNumber">{index + 4}</p>
                         </div>
                     ))}
             </div>
